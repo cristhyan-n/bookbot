8720c76f0355c1bdfa7159eacc58f63a4aab896b
cristhyan-n <cristhyan.vo@gmail.com>
2025-05-23 11:04:07 -0300

    add sys.argv to script

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-from stats import get_num_words, get_num_char
+from stats import get_num_words, get_num_char, get_order_list, get_text_form
+import sys
 
 
 def get_book_text():
-    with open("books/frankenstein.txt") as f:
-        file_contens = f.read()
-        return file_contens
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        with open(sys.argv[1]) as f:
+            file_contens = f.read()
+            return file_contens
 
 
 def main():
     file_text = get_book_text()
-    char_num = get_num_words(file_text)
-    get_num_char(char_num)
+    get_num_words(file_text)
+    char_num = get_text_form(file_text)
+    order_list = get_num_char(char_num)
+
+    print(" BOOKBOT ".center(40, "="))
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    print(" Word Count ".center(40, "-"))
+    print(f"Found {len(file_text.split())} total words")
+    print(" Character Count ".center(40, "-"))
+
+    get_order_list(order_list)
+    print(" END ".center(40, "-"))
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,10 +1,16 @@
 dict_char = {}
+ordened_list = []
 
 
-def get_num_words(text):
+def get_text_form(text):
     text_number = text.split()
     replace = " ".join(text_number).lower()
     return replace
+
+
+def get_num_words(text_form):
+    words = len(text_form.split())
+    return words
 
 
 def get_num_char(char_num):
@@ -12,5 +18,27 @@ def get_num_char(char_num):
         dict_char.setdefault(i, 0)
         if i in dict_char:
             dict_char[i] += 1
+    return dict_char
 
-    return print(dict_char)
+
+def get_order_list(dict_char):
+
+    for dict in dict_char:
+        key = dict
+        value = dict_char[dict]
+        dict_list = {"char": key, "num": value}
+        ordened_list.append(dict_list)
+    ordened_list.sort(reverse=True, key=sort_on)
+    for i in ordened_list:
+        for item in i:
+            if item == "char":
+                new_key = i[item]
+            else:
+                new_value = i[item]
+
+        if new_key.isalpha():
+            print(f"{new_key}: {new_value}")
+
+
+def sort_on(ordened_list):
+    return ordened_list["num"]
